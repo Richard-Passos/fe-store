a067e618a2eb69ee415c54bf56e6f46aae1dea96
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-03 15:47:05 -0300

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,77 @@
+const getColor = (cssVar) => `hsl(var(${cssVar}))`;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.jsx',
+    './src/components/**/*.jsx',
+    './src/patterns/**/*.jsx',
+    './src/views/**/*.jsx',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        base: getColor('--base'),
+        content: getColor('--content'),
+        muted: {
+          DEFAULT: getColor('--muted'),
+          content: getColor('--muted-c'),
+        },
+        primary: {
+          DEFAULT: getColor('--primary'),
+          content: getColor('--primary-c'),
+        },
+        secondary: {
+          DEFAULT: getColor('--secondary'),
+          content: getColor('--secondary-c'),
+        },
+        accent: {
+          DEFAULT: getColor('--accent'),
+          content: getColor('--accent-c'),
+        },
+        info: {
+          DEFAULT: getColor('--info'),
+          content: getColor('--info-c'),
+        },
+        warning: {
+          DEFAULT: getColor('--warning'),
+          content: getColor('--warning-c'),
+        },
+        danger: {
+          DEFAULT: getColor('--danger'),
+          content: getColor('--danger-c'),
+        },
+        current: 'currentColor',
+      },
+      fontSize: {
+        base: '',
+        md: ['1rem', '1.5rem']
+      },
+      borderRadius: {
+        sm: 'var(--radius-sm)',
+        md: 'var(--radius-md)',
+        lg: 'var(--radius-lg)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      data: {
+        closed: 'state=closed',
+        open: 'state=open',
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require('tailwindcss-animate')],
+};
