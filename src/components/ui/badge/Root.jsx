92ba4f9f55eebb5af6b8e02bcdc915b9c95d0d33
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-12 19:46:57 -0300

    Refactoring badge design and code

--- src/components/ui/badge/Root.jsx
+++ src/components/ui/badge/Root.jsx
@@ -11,18 +11,19 @@ const Badge = ({ variants, className, ...props }) => {
 };
 
 const badgeVariants = cnv({
-  base: 'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors border-transparent',
+  base: 'inline-flex items-center rounded-full border border-transparent px-2.5 py-0.5 text-xs font-semibold transition-colors',
   variants: {
     color: {
       primary: '[--base:--primary] [--content:--primary-c]',
       secondary: '[--base:--secondary] [--content:--secondary-c]',
+      accent: '[--base:--accent] [--content:--accent-c]',
       danger: '[--base:--danger] [--content:--danger-c]',
-      base: '[--base:--base] [--content:--content]',
+      base: '[--base] [--content]',
     },
     variant: {
-      filled: 'bg-base text-content hover:bg-base/80',
-      outline: 'border-base text-base hover:bg-base hover:text-content',
-      ghost: 'text-base hover:bg-base/20 hover:text-content',
+      filled: 'bg-base text-content',
+      outline: 'border-base text-base',
+      ghost: 'bg-base/20 text-content',
     },
   },
   defaultVariants: {
