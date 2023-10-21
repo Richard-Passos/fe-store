503fa6b3fa9f2ec861e72847d0af3fade35607e4
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-21 15:23:04 -0300

    Creating popover structure

---
+++ src/components/ui/popover/index.js
@@ -0,0 +1,14 @@
+import Close from './Close';
+import Content from './Content';
+import Portal from './Portal';
+import Root from './Root';
+import Trigger from './Trigger';
+
+const Popover = Root;
+
+Popover.Close = Close;
+Popover.Content = Content;
+Popover.Portal = Portal;
+Popover.Trigger = Trigger;
+
+export default Popover;
