f1379d06f2a8b2c2a1579af10b9187143ddcf46d
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-21 15:25:25 -0300

    Refactoring input design

--- src/components/ui/input/Root.jsx
+++ src/components/ui/input/Root.jsx
@@ -6,7 +6,7 @@ const Input = ({ className, ...props }, ref) => {
   return (
     <input
       className={cn(
-        'flex h-10 w-full border-b bg-main px-4 transition-colors file:border-none file:bg-transparent file:font-medium placeholder:text-muted-content focus:border-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+        'flex h-10 w-full rounded-sm border bg-main px-4 text-sm transition-colors file:border-none file:font-medium placeholder:text-muted-content disabled:cursor-not-allowed disabled:opacity-50',
         className,
       )}
       ref={ref}
