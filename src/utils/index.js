05e15da6bbd6742d881ee4791541da70f2b0bb0a
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-23 14:31:25 -0300

    Slight changes

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,6 @@ export { default as capitalize } from './capitalize';
 export { default as cn } from './cn';
 export { default as cnv } from './cnv';
 export { default as isExternalUrl } from './isExternalUrl';
+export { default as isFunctionThanCall } from './isFunctionThanCall';
 export { default as normalizeCompName } from './normalizeCompName';
+export { default as setRefs } from './setRefs';
--- src/views/home/Root.jsx
+++ src/views/home/Root.jsx
@@ -1,133 +1,39 @@
-import { X } from 'lucide-react';
+'use client';
 
-import { Button, Dialog, Input, Label, Popover } from '@/components/ui';
+import { useEffect, useState } from 'react';
 
-const Home = () => {
+import { Progress } from '@/components/ui';
+import variantsColor from '@/components/ui/variantsColor';
+
+const getKeys = (obj) => Object.keys(obj);
+
+const progressVariants = [
+  ...getKeys(variantsColor).map((key) => ({ color: key, size: 'sm' })),
+  ...getKeys(variantsColor).map((key) => ({ color: key, size: 'md' })),
+  ...getKeys(variantsColor).map((key) => ({ color: key, size: 'lg' })),
+];
+
+export default function ProgressDemo() {
+  const [progress, setProgress] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <main className='mt-48 flex w-full flex-col items-center gap-48'>
-      <Popover>
-        <Popover.Trigger asChild>
-          <Button variants={{ variant: 'outline' }}>Open popover</Button>
-        </Popover.Trigger>
-
-        <Popover.Portal>
-          <Popover.Content className='grid gap-5'>
-            <header className='space-y-2'>
-              <h4 className='font-medium leading-none'>Dimensions</h4>
-
-              <p className='text-sm text-muted-content'>
-                Set the dimensions for the layer.
-              </p>
-            </header>
-
-            <div className='grid gap-2'>
-              <div className='grid grid-cols-3 items-center gap-4'>
-                <Label htmlFor='width'>Width</Label>
-
-                <Input
-                  className='col-span-2 h-8'
-                  id='width'
-                  placeholder='100%'
-                />
-              </div>
-
-              <div className='grid grid-cols-3 items-center gap-4'>
-                <Label htmlFor='maxWidth'>Max. width</Label>
-
-                <Input
-                  className='col-span-2 h-8'
-                  id='maxWidth'
-                  placeholder='300px'
-                />
-              </div>
-
-              <div className='grid grid-cols-3 items-center gap-4'>
-                <Label htmlFor='height'>Height</Label>
-
-                <Input
-                  className='col-span-2 h-8'
-                  id='height'
-                  placeholder='25px'
-                />
-              </div>
-
-              <div className='grid grid-cols-3 items-center gap-4'>
-                <Label htmlFor='maxHeight'>Max. height</Label>
-
-                <Input
-                  className='col-span-2 h-8'
-                  id='maxHeight'
-                  placeholder='none'
-                />
-              </div>
-            </div>
-
-            <Popover.Close>
-              <X />
-            </Popover.Close>
-          </Popover.Content>
-        </Popover.Portal>
-      </Popover>
-
-      <Dialog>
-        <Dialog.Trigger asChild>
-          <Button variants={{ variant: 'outline' }}>Edit Profile</Button>
-        </Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay />
-
-          <Dialog.Content className='sm:max-w-[425px]'>
-            <Dialog.Header>
-              <Dialog.Title>Edit profile</Dialog.Title>
-              <Dialog.Description>
-                Make changes to your profile here. Click save when you&apos;re
-                done.
-              </Dialog.Description>
-            </Dialog.Header>
-
-            <div className='grid gap-4 py-4'>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label
-                  className=''
-                  htmlFor='name'
-                >
-                  Name
-                </Label>
-
-                <Input
-                  className='col-span-3'
-                  defaultValue='Pedro Duarte'
-                  id='name'
-                />
-              </div>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label
-                  className=''
-                  htmlFor='username'
-                >
-                  Username
-                </Label>
-
-                <Input
-                  className='col-span-3'
-                  defaultValue='@peduarte'
-                  id='username'
-                />
-              </div>
-            </div>
-
-            <Dialog.Actions>
-              <Button type='submit'>Save changes</Button>
-            </Dialog.Actions>
-
-            <Dialog.Close>
-              <X />
-            </Dialog.Close>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog>
+    <main className='flex min-h-screen w-full items-center justify-center'>
+      <div className='w-full max-w-lg space-y-6'>
+        {progressVariants.map((variants, i) => (
+          <Progress
+            key={`Home progress ${i}`}
+            value={progress}
+            variants={variants}
+          >
+            <Progress.Indicator />
+          </Progress>
+        ))}
+      </div>
     </main>
   );
-};
-
-export default Home;
+}
