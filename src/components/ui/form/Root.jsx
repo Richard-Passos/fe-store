06b3594941c2e27c803a10015388489e87d51d99
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-24 10:00:14 -0300

    Adding foward ref

--- src/components/ui/form/Root.jsx
+++ src/components/ui/form/Root.jsx
@@ -1,19 +1,21 @@
 'use client';
 
+import { forwardRef } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { cn } from '@/utils';
 
-const Form = ({ className, onSubmit, ...props }) => {
+const Form = ({ className, onSubmit, ...props }, ref) => {
   const { handleSubmit } = useFormContext();
 
   return (
     <form
       className={cn('space-y-8', className)}
       onSubmit={handleSubmit(onSubmit)}
+      ref={ref}
       {...props}
     />
   );
 };
 
-export default Form;
+export default forwardRef(Form);
