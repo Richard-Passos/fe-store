98283216c6ded6a30905b712145291def7a5dfbf
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-25 11:14:56 -0300

    Adding corner to scroll-area

--- src/components/ui/scroll-area/index.js
+++ src/components/ui/scroll-area/index.js
@@ -1,9 +1,11 @@
+import Corner from './Corner';
 import Root from './Root';
 import Viewport from './Viewport';
 import Scrollbar from './scrollbar';
 
 const ScrollArea = Root;
 
+ScrollArea.Corner = Corner;
 ScrollArea.Viewport = Viewport;
 ScrollArea.Scrollbar = Scrollbar;
 
