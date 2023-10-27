82bd11b56265415e27a4c3e6a3b0ca95d2451285
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-26 21:09:55 -0300

    Removing unnecessary code

--- src/views/home/Root.jsx
+++ src/views/home/Root.jsx
@@ -1,38 +1,155 @@
-import { ScrollArea } from '@/components/ui';
+import { CheckIcon, ChevronDown, ChevronUp } from 'lucide-react';
 
-const TAGS = [...Array(50)].map((_, i, arr) => `v1.2.0-beta.${arr.length - i}`);
+import { Select } from '@/components/ui';
+import { cn } from '@/utils';
 
-export default function ProgressDemo() {
+const HomeView = ({ className, ...props }) => {
   return (
-    <main className='flex min-h-screen w-full max-w-bounds items-center justify-center'>
-      <ScrollArea className='h-36 w-72 border'>
-        <ScrollArea.Viewport>
-          <div className='px-4 py-5'>
-            <div className='flex'>
-              <div>Tags</div>
+    <main
+      className={cn(
+        'flex min-h-screen w-full max-w-bounds justify-center pt-48',
+        className,
+      )}
+      {...props}
+    >
+      <Select>
+        <Select.Trigger className='w-44'>
+          <Select.Value placeholder='Select a fruit…' />
 
-              {TAGS.map((tag) => (
-                <div
-                  className='Tag'
-                  key={tag}
+          <Select.Icon
+            asChild
+            className='transition-transform group-data-open:rotate-180'
+          >
+            <ChevronDown />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content>
+            <Select.Viewport>
+              <Select.Group>
+                <Select.Label>Fruits</Select.Label>
+
+                <SelectItem value='apple'>Apple</SelectItem>
+
+                <SelectItem value='banana'>Banana</SelectItem>
+
+                <SelectItem value='blueberry'>Blueberry</SelectItem>
+
+                <SelectItem value='grapes'>Grapes</SelectItem>
+
+                <SelectItem value='pineapple'>Pineapple</SelectItem>
+              </Select.Group>
+
+              <Select.Separator />
+
+              <Select.Group>
+                <Select.Label>Vegetables</Select.Label>
+                <SelectItem value='aubergine'>Aubergine</SelectItem>
+
+                <SelectItem value='broccoli'>Broccoli</SelectItem>
+
+                <SelectItem
+                  disabled
+                  value='carrot'
                 >
-                  {tag}
-                </div>
-              ))}
-            </div>
-          </div>
-        </ScrollArea.Viewport>
+                  Carrot
+                </SelectItem>
 
-        <ScrollArea.Scrollbar orientation='horizontal'>
-          <ScrollArea.Scrollbar.Thumb />
-        </ScrollArea.Scrollbar>
+                <SelectItem value='courgette'>Courgette</SelectItem>
 
-        <ScrollArea.Scrollbar>
-          <ScrollArea.Scrollbar.Thumb />
-        </ScrollArea.Scrollbar>
+                <SelectItem value='leek'>Leek</SelectItem>
+              </Select.Group>
 
-        <ScrollArea.Corner />
-      </ScrollArea>
+              <Select.Separator />
+
+              <Select.Group>
+                <Select.Label>Meat</Select.Label>
+
+                <SelectItem value='beef'>Beef</SelectItem>
+
+                <SelectItem value='chicken'>Chicken</SelectItem>
+
+                <SelectItem value='lamb'>Lamb</SelectItem>
+
+                <SelectItem value='pork'>Pork</SelectItem>
+              </Select.Group>
+
+              <Select.Separator />
+
+              <Select.Group>
+                <Select.Label>Fruits</Select.Label>
+
+                <SelectItem value='1'>Apple</SelectItem>
+
+                <SelectItem value='2'>Banana</SelectItem>
+
+                <SelectItem value='3'>Blueberry</SelectItem>
+
+                <SelectItem value='4'>Grapes</SelectItem>
+
+                <SelectItem value='5'>Pineapple</SelectItem>
+              </Select.Group>
+
+              <Select.Separator />
+
+              <Select.Group>
+                <Select.Label>Vegetables</Select.Label>
+                <SelectItem value='6'>Aubergine</SelectItem>
+
+                <SelectItem value='7'>Broccoli</SelectItem>
+
+                <SelectItem
+                  disabled
+                  value='8'
+                >
+                  Carrot
+                </SelectItem>
+
+                <SelectItem value='9'>Courgette</SelectItem>
+
+                <SelectItem value='10'>Leek</SelectItem>
+              </Select.Group>
+
+              <Select.Separator />
+
+              <Select.Group>
+                <Select.Label>Meat</Select.Label>
+
+                <SelectItem value='11'>Beef</SelectItem>
+
+                <SelectItem value='12'>Chicken</SelectItem>
+
+                <SelectItem value='13'>Lamb</SelectItem>
+
+                <SelectItem value='14'>Pork</SelectItem>
+              </Select.Group>
+            </Select.Viewport>
+
+            <Select.ScrollButton.Up>
+              <ChevronUp className='h-3.5 w-3.5' />
+            </Select.ScrollButton.Up>
+
+            <Select.ScrollButton.Down>
+              <ChevronDown className='h-3.5 w-3.5' />
+            </Select.ScrollButton.Down>
+          </Select.Content>
+        </Select.Portal>
+      </Select>
     </main>
   );
-}
+};
+
+const SelectItem = ({ children, ...props }) => {
+  return (
+    <Select.Item {...props}>
+      <Select.Item.Text>{children}</Select.Item.Text>
+
+      <Select.Item.Indicator>
+        <CheckIcon />
+      </Select.Item.Indicator>
+    </Select.Item>
+  );
+};
+
+export default HomeView;
