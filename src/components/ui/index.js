0b3adcde675528901a592c5baf9453dfdfefd6c2
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-26 21:09:12 -0300

    Importing and exporting select

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -25,5 +25,6 @@ export { default as Popover } from './popover';
 export { default as Progress } from './progress';
 export { default as RadioGroup } from './radio-group';
 export { default as ScrollArea } from './scroll-area';
+export { default as Select } from './select';
 export { default as Svg } from './svg';
 export { default as Text } from './text';
