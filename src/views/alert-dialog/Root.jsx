fc0fe71c194090463754aca52c21a7e4f10de46e
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:22:46 -0300

    Refacotring alert-dialog demo code and design

--- src/views/alert-dialog/Root.jsx
+++ src/views/alert-dialog/Root.jsx
@@ -1,52 +1,43 @@
 import { AlertDialog, Button } from '@/components/ui';
-import { cn } from '@/utils';
 
-const AlertDialogView = ({ className, ...props }) => {
+const AlertDialogView = (props) => {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen w-full max-w-bounds justify-center pt-48',
-        className,
-      )}
-      {...props}
-    >
-      <AlertDialog>
-        <AlertDialog.Trigger asChild>
-          <Button variants={{ variant: 'outline' }}>Show Dialog</Button>
-        </AlertDialog.Trigger>
+    <AlertDialog {...props}>
+      <AlertDialog.Trigger asChild>
+        <Button variants={{ variant: 'outline' }}>Show dialog</Button>
+      </AlertDialog.Trigger>
 
-        <AlertDialog.Portal>
-          <AlertDialog.Overlay />
+      <AlertDialog.Portal>
+        <AlertDialog.Overlay />
 
-          <AlertDialog.Content>
-            <AlertDialog.Header>
-              <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+        <AlertDialog.Content>
+          <AlertDialog.Header>
+            <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
 
-              <AlertDialog.Description>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </AlertDialog.Description>
-            </AlertDialog.Header>
+            <AlertDialog.Description>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialog.Description>
+          </AlertDialog.Header>
 
-            <AlertDialog.Actions>
-              <AlertDialog.Cancel asChild>
-                <Button
-                  variants={{ color: 'inverted', variant: 'ghost', size: 'sm' }}
-                >
-                  Cancel
-                </Button>
-              </AlertDialog.Cancel>
+          <AlertDialog.Actions>
+            <AlertDialog.Cancel asChild>
+              <Button
+                variants={{ color: 'inverted', variant: 'ghost', size: 'sm' }}
+              >
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
 
-              <AlertDialog.Action asChild>
-                <Button variants={{ color: 'danger', size: 'sm' }}>
-                  Continue
-                </Button>
-              </AlertDialog.Action>
-            </AlertDialog.Actions>
-          </AlertDialog.Content>
-        </AlertDialog.Portal>
-      </AlertDialog>
-    </main>
+            <AlertDialog.Action asChild>
+              <Button variants={{ color: 'danger', size: 'sm' }}>
+                Continue
+              </Button>
+            </AlertDialog.Action>
+          </AlertDialog.Actions>
+        </AlertDialog.Content>
+      </AlertDialog.Portal>
+    </AlertDialog>
   );
 };
 
