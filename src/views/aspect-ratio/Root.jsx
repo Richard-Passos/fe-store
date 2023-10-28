782c626abe0864dc3615e44e3b12038aadaa5388
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:23:14 -0300

    Refacotring aspect-ratio demo code and design

--- src/views/aspect-ratio/Root.jsx
+++ src/views/aspect-ratio/Root.jsx
@@ -1,29 +1,65 @@
-import { AspectRatio, Image } from '@/components/ui';
+import { AspectRatio, Image, Text } from '@/components/ui';
 import { cn } from '@/utils';
 
-const DialogView = ({ className, ...props }) => {
+const ASPECT_RATIO_VARIANTS = [
+  { ratio: 16 / 9 },
+  { ratio: 4 / 3 },
+  { ratio: 3 / 2 },
+  { ratio: 5 / 3 },
+  { ratio: 5 / 4 },
+  { ratio: 1 },
+];
+
+const AspectRatioView = ({ className, ...props }) => {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen w-full max-w-bounds items-center justify-center',
-        className,
-      )}
+    <ul
+      className={cn('flex w-full flex-wrap justify-center gap-12', className)}
       {...props}
     >
-      <div className='w-full max-w-lg'>
-        <AspectRatio ratio={16 / 9}>
-          <Image
-            alt='A road winding through a forest near the ocean.'
-            className='rounded-md object-cover'
-            fill
-            priority
-            quality={100}
-            src='/images/demo.png'
-          />
-        </AspectRatio>
-      </div>
-    </main>
+      {ASPECT_RATIO_VARIANTS.map((variants) => (
+        <li
+          className='h-fit w-full max-w-lg space-y-6 rounded-md bg-muted p-5 shadow-md'
+          key={getKeyValue(variants).join()}
+        >
+          <Title variants={variants} />
+
+          <AspectRatio {...variants}>
+            <Image
+              alt='A road winding through a forest near the ocean.'
+              className='rounded-sm object-cover'
+              fill
+              priority
+              quality={100}
+              src='/images/demo.png'
+            />
+          </AspectRatio>
+        </li>
+      ))}
+    </ul>
   );
 };
 
-export default DialogView;
+const getKeyValue = (obj) => Object.entries(obj);
+
+const Title = ({ variants, className, ...props }) => {
+  return (
+    <Text.Title
+      className={cn('text-base font-medium', className)}
+      {...props}
+    >
+      {getKeyValue(variants).map(([key, value], i, arr) => (
+        <>
+          <span className='first:capitalize'>{key}</span>:{' '}
+          <span className='text-muted-content'>
+            {key === 'ratio'
+              ? value.toString().replace(/([.]\d\d).*/, '$1') + ':1'
+              : value.toString()}
+          </span>
+          {i < arr.length - 1 && ' - '}
+        </>
+      ))}
+    </Text.Title>
+  );
+};
+
+export default AspectRatioView;
