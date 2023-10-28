89e334aca324101b2141d3e6d1d50386d24ceef6
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:23:02 -0300

    Refacotring avatar demo code and design

--- src/views/avatar/Root.jsx
+++ src/views/avatar/Root.jsx
@@ -1,27 +1,18 @@
 import { Avatar } from '@/components/ui';
-import { cn } from '@/utils';
 
-const DialogView = ({ className, ...props }) => {
+const AvatarView = ({ className, ...props }) => {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen w-full max-w-bounds items-center justify-center',
-        className,
-      )}
-      {...props}
-    >
-      <Avatar>
-        <Avatar.Image
-          alt='A road winding through a forest near the ocean.'
-          height={902}
-          src='/images/demo.png'
-          width={600}
-        />
+    <Avatar {...props}>
+      <Avatar.Image
+        alt='A road winding through a forest near the ocean.'
+        height={902}
+        src='/images/demo.png'
+        width={600}
+      />
 
-        <Avatar.Fallback>RF</Avatar.Fallback>
-      </Avatar>
-    </main>
+      <Avatar.Fallback>RF</Avatar.Fallback>
+    </Avatar>
   );
 };
 
-export default DialogView;
+export default AvatarView;
