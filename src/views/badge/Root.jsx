75f206506808339838e06fa0861fd920cc86f389
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:23:24 -0300

    Refacotring badge demo code and design

--- src/views/badge/Root.jsx
+++ src/views/badge/Root.jsx
@@ -1,36 +1,55 @@
-import { Badge } from '@/components/ui';
+import { Badge, Text } from '@/components/ui';
 import variantsColor from '@/components/ui/variantsColor';
-import { capitalize, cn } from '@/utils';
+import { cn } from '@/utils';
 
-const BADGE_VARIANTS = [
-  ...Object.keys(variantsColor).map((color) => ({ color, variant: 'filled' })),
-  ...Object.keys(variantsColor).map((color) => ({ color, variant: 'outline' })),
-  ...Object.keys(variantsColor).map((color) => ({ color, variant: 'ghost' })),
-];
+const BADGE_COLOR_VARIANTS = Object.keys(variantsColor),
+  BADGE_VARIANT_VARIANTS = ['filled', 'outline', 'ghost'];
 
-const DialogView = ({ className, ...props }) => {
+const BADGE_VARIANTS = BADGE_COLOR_VARIANTS.map((color) =>
+  BADGE_VARIANT_VARIANTS.map((variant) => ({
+    color,
+    variant,
+  })),
+)
+  .reduce((arr, variants) => [...arr, ...variants], [])
+  .sort((a, b) => a.variant.localeCompare(b.variant));
+
+const BadgeView = ({ className, ...props }) => {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen w-full max-w-bounds flex-col items-center justify-center gap-24 py-48',
-        className,
-      )}
+    <ul
+      className={cn('flex flex-wrap justify-center gap-12', className)}
       {...props}
     >
-      {BADGE_VARIANTS.map(({ color, variant }) => (
-        <section
-          className='w-full max-w-xs space-y-6'
-          key={color + variant}
+      {BADGE_VARIANTS.map((variants) => (
+        <li
+          className='w-full max-w-xs space-y-6 rounded-md bg-muted p-5 shadow-md'
+          key={getKeyValue(variants).join()}
         >
-          <h2 className='font-medium'>
-            {capitalize(color)} - {variant}
-          </h2>
+          <Title variants={variants} />
 
-          <Badge variants={{ color, variant }}>Badge</Badge>
-        </section>
+          <Badge variants={variants}>Badge</Badge>
+        </li>
       ))}
-    </main>
+    </ul>
   );
 };
 
-export default DialogView;
+const getKeyValue = (obj) => Object.entries(obj);
+
+const Title = ({ variants, className, ...props }) => {
+  return (
+    <Text.Title
+      className={cn('text-base font-medium', className)}
+      {...props}
+    >
+      {getKeyValue(variants).map(([key, value], i, arr) => (
+        <>
+          <span className='first:capitalize'>{key}</span>:{' '}
+          <span className='text-muted-content'>{value.toString()}</span>
+          {i < arr.length - 1 && ' - '}
+        </>
+      ))}
+    </Text.Title>
+  );
+};
+export default BadgeView;
