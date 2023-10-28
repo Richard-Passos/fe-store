5a6db1463c93d84026e580cdba1bc152edc30ed2
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:20:05 -0300

    Adding a global design

--- src/app/components/[component]/page.jsx
+++ src/app/components/[component]/page.jsx
@@ -1,10 +1,27 @@
-import { normalizeCompName } from '@/utils';
+import { Text } from '@/components/ui';
+import { capitalize, normalizeCompName } from '@/utils';
 import * as Views from '@/views';
 
 const HomePage = ({ params: { component } }) => {
-  const View = Views[normalizeCompName(component)] ?? Views.Home;
+  const Component = Views[normalizeCompName(component)],
+    isComponent = Boolean(Component);
 
-  return <View />;
+  const View = Component ?? Views.Home;
+
+  return isComponent ? (
+    <main className='mx-auto flex min-h-screen w-full max-w-bounds flex-col items-center gap-24 py-24'>
+      <Text.Title
+        asChild
+        className='text-center text-5xl'
+      >
+        <h1>{capitalize(component.replace(/[-_]/, ' '))}</h1>
+      </Text.Title>
+
+      <View />
+    </main>
+  ) : (
+    <View />
+  );
 };
 
 export default HomePage;
