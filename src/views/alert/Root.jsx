5beef0c7b785ad999951897ebd6c6a719b2a2075
Richard Passos <richard.ac.passos12@gmail.com>
2023-10-27 21:22:39 -0300

    Refacotring alert demo code and design

--- src/views/alert/Root.jsx
+++ src/views/alert/Root.jsx
@@ -1,33 +1,35 @@
 import { Terminal } from 'lucide-react';
 
-import { Alert } from '@/components/ui';
+import { Alert, Text } from '@/components/ui';
 import variantsColor from '@/components/ui/variantsColor';
-import { capitalize, cn } from '@/utils';
+import { cn } from '@/utils';
 
-const ALERT_VARIANTS = [
-  ...Object.keys(variantsColor).map((color) => ({ color, variant: 'filled' })),
-  ...Object.keys(variantsColor).map((color) => ({ color, variant: 'outline' })),
-];
+const ALERT_COLOR_VARIANTS = Object.keys(variantsColor),
+  ALERT_VARIANT_VARIANTS = ['filled', 'outline'];
+
+const ALERT_VARIANTS = ALERT_COLOR_VARIANTS.map((color) =>
+  ALERT_VARIANT_VARIANTS.map((variant) => ({
+    color,
+    variant,
+  })),
+)
+  .reduce((arr, variants) => [...arr, ...variants], [])
+  .sort((a, b) => a.variant.localeCompare(b.variant));
 
 const AlertView = ({ className, ...props }) => {
   return (
-    <main
-      className={cn(
-        'flex min-h-screen w-full max-w-bounds flex-col items-center justify-center gap-24 py-48',
-        className,
-      )}
+    <ul
+      className={cn('flex flex-wrap justify-center gap-12', className)}
       {...props}
     >
-      {ALERT_VARIANTS.map(({ color, variant }) => (
-        <section
-          className='space-y-6'
-          key={color + variant}
+      {ALERT_VARIANTS.map((variants) => (
+        <li
+          className='space-y-6 rounded-md bg-muted p-5 shadow-md'
+          key={getKeyValue(variants).join()}
         >
-          <h2 className='font-medium'>
-            {capitalize(color)} - {variant}
-          </h2>
+          <Title variants={variants} />
 
-          <Alert variants={{ color, variant }}>
+          <Alert variants={variants}>
             <Alert.Icon>
               <Terminal />
             </Alert.Icon>
@@ -38,9 +40,28 @@ const AlertView = ({ className, ...props }) => {
               You can add components to your app using the cli.
             </Alert.Description>
           </Alert>
-        </section>
+        </li>
       ))}
-    </main>
+    </ul>
+  );
+};
+
+const getKeyValue = (obj) => Object.entries(obj);
+
+const Title = ({ variants, className, ...props }) => {
+  return (
+    <Text.Title
+      className={cn('text-base font-medium', className)}
+      {...props}
+    >
+      {getKeyValue(variants).map(([key, value], i, arr) => (
+        <>
+          <span className='first:capitalize'>{key}</span>:{' '}
+          <span className='text-muted-content'>{value.toString()}</span>
+          {i < arr.length - 1 && ' - '}
+        </>
+      ))}
+    </Text.Title>
   );
 };
 
