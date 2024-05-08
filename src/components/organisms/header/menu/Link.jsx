09f868dcf34452501c6319974deb593840eacac0
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-08 20:25:41 -0300

    CHORE remove unnecessary rich

--- src/components/organisms/header/menu/Link.jsx
+++ src/components/organisms/header/menu/Link.jsx
@@ -5,7 +5,7 @@ const HeaderMenuLink = ({ isActive, className, ...props }) => {
   return (
     <Link
       className={cn(
-        'relative flex h-10 w-full justify-start rounded-sm px-6 no-underline transition-none hover:bg-active',
+        'relative flex h-10 w-full justify-start rounded-sm px-6 transition-none before:hidden hover:bg-active',
         isActive &&
           'before:absolute before:left-3 before:h-1/3 before:w-0.5 before:-translate-x-1/2 before:rounded-full before:bg-primary',
         className
