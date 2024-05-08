75ef997f7df3f2deb2ecf0255ccfc8db51917952
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-08 20:27:24 -0300

    CHORE tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,111 +2,139 @@ const getColor = (cssVar) => `hsl(var(${cssVar}))`;
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: [
     './src/app/**/*.jsx',
     './src/components/**/*.jsx',
-    './src/components-demo/**/*.jsx',
-    './src/components/ui/variantColors.js',
-    './src/views/**/*.jsx',
+    './src/components/**/variants.js',
+    './src/components/*Variants.js',
+    './src/styles/*.css'
   ],
   theme: {
     extend: {
       spacing: {
-        px:'0.8px',
-        'font-blank-space': 'var(--font-blank-space)',
+        px: '0.8px',
+        '9/10': '90%',
+        xs: 'var(--spacing-xs)',
+        sm: 'var(--spacing-sm)',
+        md: 'var(--spacing-md)',
+        lg: 'var(--spacing-lg)',
+        xl: 'var(--spacing-xl)'
       },
-
       colors: {
-        main: getColor('--main'),
-        content: getColor('--content'),
+        main: getColor('--root-main'),
+        active: getColor('--root-active'),
+        content: getColor('--root-content'),
+        muted: getColor('--root-muted'),
         inverted: {
-          DEFAULT: getColor('--inverted'),
-          content: getColor('--inverted-c'),
-        },
-        muted: {
-          DEFAULT: getColor('--muted'),
-          content: getColor('--muted-c'),
+          DEFAULT: getColor('--root-inverted'),
+          active: getColor('--root-inverted-a'),
+          content: getColor('--root-inverted-c'),
+          muted: getColor('--root-inverted-m')
         },
         primary: {
-          DEFAULT: getColor('--primary'),
-          content: getColor('--primary-c'),
+          DEFAULT: getColor('--root-primary'),
+          active: getColor('--root-primary-a'),
+          content: getColor('--root-primary-c'),
+          muted: getColor('--root-primary-m')
         },
         secondary: {
-          DEFAULT: getColor('--secondary'),
-          content: getColor('--secondary-c'),
+          DEFAULT: getColor('--root-secondary'),
+          active: getColor('--root-secondary-a'),
+          content: getColor('--root-secondary-c'),
+          muted: getColor('--root-secondary-m')
         },
         accent: {
-          DEFAULT: getColor('--accent'),
-          content: getColor('--accent-c'),
+          DEFAULT: getColor('--root-accent'),
+          active: getColor('--root-accent-a'),
+          content: getColor('--root-accent-c'),
+          muted: getColor('--root-accent-m')
         },
         info: {
-          DEFAULT: getColor('--info'),
-          content: getColor('--info-c'),
+          DEFAULT: getColor('--root-info'),
+          active: getColor('--root-info-a'),
+          content: getColor('--root-info-c'),
+          muted: getColor('--root-info-m')
         },
         success: {
-          DEFAULT: getColor('--success'),
-          content: getColor('--success-c'),
+          DEFAULT: getColor('--root-success'),
+          active: getColor('--root-success-a'),
+          content: getColor('--root-success-c'),
+          muted: getColor('--root-success-m')
         },
         warning: {
-          DEFAULT: getColor('--warning'),
-          content: getColor('--warning-c'),
+          DEFAULT: getColor('--root-warning'),
+          active: getColor('--root-warning-a'),
+          content: getColor('--root-warning-c'),
+          muted: getColor('--root-warning-m')
         },
         danger: {
-          DEFAULT: getColor('--danger'),
-          content: getColor('--danger-c'),
+          DEFAULT: getColor('--root-danger'),
+          active: getColor('--root-danger-a'),
+          content: getColor('--root-danger-c'),
+          muted: getColor('--root-danger-m')
         },
-        border: 'hsl(var(--muted-c)/.2)',
-        current: 'currentColor',
+        border: getColor('--root-border'),
+        current: 'currentColor'
       },
       borderColor: {
-        DEFAULT: 'hsl(var(--muted-c)/.2)',
+        DEFAULT: getColor('--root-border')
       },
       borderRadius: {
         sm: 'var(--radius-sm)',
         md: 'var(--radius-md)',
         lg: 'var(--radius-lg)',
-        inherit: 'inherit',
+        inherit: 'inherit'
       },
       outlineColor: {
-        DEFAULT: 'currentColor',
+        DEFAULT: 'currentColor'
       },
       outlineOffset: {
-        DEFAULT: '2',
+        DEFAULT: '2'
       },
       outlineWidth: {
-        DEFAULT: 2,
+        DEFAULT: 2
+      },
+      minHeight: {
+        bounds: 'var(--max-h)'
+      },
+      minWidth: {
+        bounds: 'var(--max-w)'
       },
       maxHeight: {
-        bounds: 'var(--max-h)',
+        bounds: 'var(--max-h)'
       },
       maxWidth: {
-        bounds: 'var(--max-w)',
+        bounds: 'var(--max-w)'
       },
       keyframes: {
         'slide-down': {
           from: { height: 0 },
-          to: { height: 'var(--height, 0)' },
+          to: { height: 'var(--height, 0)' }
         },
         'slide-up': {
           from: { height: 'var(--height, 0)' },
-          to: { height: 0 },
-        },
+          to: { height: 0 }
+        }
       },
       animation: {
         'slide-down': 'slide-down 300ms cubic-bezier(0.87, 0, 0.13, 1)',
-        'slide-up': 'slide-up 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+        'slide-up': 'slide-up 300ms cubic-bezier(0.87, 0, 0.13, 1)'
       },
       fontFamily: {
-        app: 'var(--font-app)',
+        app: 'var(--font-app)'
       },
       data: {
+        active: 'state=active',
         checked: 'state=checked',
         closed: 'state=closed',
-        open: 'state=open',
         disabled: 'disabled',
+        open: 'state=open'
       },
-    },
+      transitionTimingFunction: {
+        backOut: 'cubic-bezier(0.34, 1.56, 0.64, 1)'
+      }
+    }
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate')]
 };
