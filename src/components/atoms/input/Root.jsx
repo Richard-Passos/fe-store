d1b2aeec6d8cc3f90e5998b121e4769c84a265a8
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 12:41:11 -0300

    CHORE add variants

--- src/components/atoms/input/Root.jsx
+++ src/components/atoms/input/Root.jsx
@@ -1,14 +1,19 @@
 import { forwardRef } from 'react';
 
+import control from '@/components/controlVariants';
 import { cn } from '@/utils';
 
-const Input = ({ className, ...props }, ref) => {
+const Input = ({ color, variant, className, ...props }, ref) => {
   return (
     <input
-      className={cn(
-        'h-10 w-full cursor-pointer rounded-sm border bg-main px-4 text-sm file:mr-2 file:mt-2 file:cursor-pointer file:border-0 file:bg-transparent file:px-0 file:font-medium file:text-content placeholder:text-content/75 disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-0 [&[type=color]]:overflow-hidden [&[type=color]]:px-0 [&[type=file]]:text-content/75',
-        className
-      )}
+      className={control({
+        color,
+        variant,
+        className: cn(
+          'file:mr-2 file:mt-2 file:cursor-pointer file:border-0 file:bg-transparent file:px-0 file:font-medium file:text-content disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-0 [&[variant=color]]:overflow-hidden [&[variant=color]]:px-0 [&[variant=file]]:text-content/75',
+          className
+        )
+      })}
       ref={ref}
       {...props}
     />
