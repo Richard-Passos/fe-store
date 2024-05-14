e0997137b113a97bd9510044f00f559fdf7bcaeb
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 14:50:54 -0300

    CHORE remove unnecessary code

--- src/components/organisms/header/menu/Trigger.jsx
+++ src/components/organisms/header/menu/Trigger.jsx
@@ -1,6 +1,6 @@
 import { useTranslations } from 'next-intl';
 
-import { Button, Icon, Text } from '@/components/atoms';
+import { Icon } from '@/components/atoms';
 import { SheetTrigger } from '@/components/molecules/sheet';
 import { cn } from '@/utils';
 
@@ -8,25 +8,19 @@ const HeaderMenuTrigger = ({ className, ...props }) => {
   const t = useTranslations('header.menu.action');
 
   return (
-    <SheetTrigger asChild>
-      <Button
-        className={cn(
-          'group relative aspect-square rounded-sm px-0',
-          className
-        )}
-        color={t('color')}
-        size={t('size')}
-        variant={t('variant')}
-        {...props}
-      >
-        <Icon
-          className='size-1/2'
-          color={t('icon.color')}
-          src={t('icon.src')}
-        />
-
-        <Text.Root className='sr-only'>{t('label')}</Text.Root>
-      </Button>
+    <SheetTrigger
+      aria-label={t('label')}
+      className={cn('aspect-square px-0', className)}
+      color={t('color')}
+      size={t('size')}
+      variant={t('variant')}
+      {...props}
+    >
+      <Icon
+        className='size-1/2'
+        color={t('icon.color')}
+        src={t('icon.src')}
+      />
     </SheetTrigger>
   );
 };
