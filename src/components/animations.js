0031b637b3ba9d18bc16ce6a5eacd5abca70b1f7
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 12:07:36 -0300

    FEAT add animations

---
+++ src/components/animations.js
@@ -0,0 +1,9 @@
+const animations = {
+  rotate135:
+    'animate-[enter_300ms_forwards] ease-backOut spin-in-[135deg] [--anim:exit_300ms_forwards] spin-out-[135deg]',
+  rotate180:
+    'animate-[enter_300ms_forwards] ease-backOut spin-in-[180deg] [--anim:exit_300ms_forwards] spin-out-[180deg]'
+};
+
+export default animations;
+export const { rotate135, rotate180 } = animations;
