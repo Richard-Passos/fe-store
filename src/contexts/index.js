27fb5de757e5919dbced2ba17278e8a6762874d7
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 12:18:49 -0300

    CHORE export new contexts

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -3,3 +3,4 @@ export { default as ErrorContext, ErrorProvider } from './Error';
 export { default as FormFieldContext, FormFieldProvider } from './FormField';
 export { default as ProgressContext, ProgressProvider } from './Progress';
 export { default as TimerbarContext, TimerbarProvider } from './Timerbar';
+export { default as ToastContext, ToastProvider } from './Toast';
