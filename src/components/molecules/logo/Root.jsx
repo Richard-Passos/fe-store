5df91af5a7da7e9a7d8de16ce78428547b5f964f
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 14:49:32 -0300

    REFA

--- src/components/molecules/logo/Root.jsx
+++ src/components/molecules/logo/Root.jsx
@@ -1,21 +1,27 @@
 import { useTranslations } from 'next-intl';
 
 import { Icon, Link } from '@/components/atoms';
+import { button } from '@/components/atoms/button';
 import { cn } from '@/utils';
 
-const Root = ({ className, ...props }) => {
+const Root = ({ color, variant, size, className, ...props }) => {
   const t = useTranslations('personalInfo.logo');
 
   return (
     <Link
-      className={cn(
-        'size-10 overflow-hidden rounded-sm p-1 transition-none',
-        className,
-      )}
+      className={button({
+        color: color ?? t('color'),
+        variant: variant ?? t('variant'),
+        size: size ?? t('size'),
+        className: cn('aspect-square p-1 before:hidden', className)
+      })}
       href='/'
       {...props}
     >
-      <Icon src={t('src')} />
+      <Icon
+        color='current'
+        src={t('src')}
+      />
     </Link>
   );
 };
