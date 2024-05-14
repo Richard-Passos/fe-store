5deac884b6778c3132c7a07b40d229e5217f0a9d
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-14 12:15:00 -0300

    CHORE remove font-blank

--- src/utils/cn.js
+++ src/utils/cn.js
@@ -3,9 +3,9 @@ import { extendTailwindMerge } from 'tailwind-merge';
 const cn = extendTailwindMerge({
   extend: {
     theme: {
-      spacing: ['font-blank-space', '9/10', 'xs', 'sm', 'md', 'lg'],
-    },
-  },
+      spacing: ['9/10', 'xs', 'sm', 'md', 'lg']
+    }
+  }
 });
 
 export default cn;
