2df067cd557de4b7d3bf68e35c3dd2048304d9bd
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 20:09:28 -0300

    CHORE add new header remove icon

--- src/components/organisms/demos/alert/Root.jsx
+++ src/components/organisms/demos/alert/Root.jsx
@@ -3,15 +3,13 @@ import { useTranslations } from 'next-intl';
 import { Icon } from '@/components/atoms';
 import colors from '@/components/colors';
 import { Alert } from '@/components/molecules';
-import { alertVariants } from '@/components/molecules/alert';
 import { renderComp } from '@/utils';
 import variantsComb from '@/utils/variantsComb';
 
 import Demos from '../Root';
 
 const ALERT = {
-  color: Object.keys(colors),
-  variant: Object.keys(alertVariants)
+  color: Object.keys(colors)
 };
 
 const DemosAlertOrganism = ({ namespace, ...props }) => {
@@ -28,22 +26,22 @@ const DemosAlertOrganism = ({ namespace, ...props }) => {
         {...variants}
       >
         {renderComp(
-          <Alert.Icon>
-            <div>
-              <Icon
-                color={t('icon.color')}
-                src={t('icon.src')}
-              />
-            </div>
-          </Alert.Icon>,
+          <div className='size-5'>
+            <Icon
+              color={t('icon.color')}
+              src={t('icon.src')}
+            />
+          </div>,
           [t('icon.src')]
         )}
 
-        {renderComp(<Alert.Title>{t('title')}</Alert.Title>, [t('title')])}
+        <Alert.Header>
+          {renderComp(<Alert.Title>{t('title')}</Alert.Title>, [t('title')])}
 
-        <Alert.Description className='mt-1.5'>
-          {t('description')}
-        </Alert.Description>
+          <Alert.Description className='mt-1.5'>
+            {t('description')}
+          </Alert.Description>
+        </Alert.Header>
       </Alert.Root>
     </Demos>
   ));
