943e297872d13c4a1368ed3e056951f37625b41f
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 19:15:03 -0300

    REFA

--- src/components/organisms/header/menu/Link.jsx
+++ src/components/organisms/header/menu/Link.jsx
@@ -5,7 +5,7 @@ const HeaderMenuLink = ({ isActive, className, ...props }) => {
   return (
     <Link
       className={cn(
-        'relative flex h-10 w-full items-center justify-start rounded-sm px-6 transition-none before:hidden hover:bg-active',
+        'relative flex h-10 w-full items-center justify-start rounded-sm px-6 text-content text-opacity-75 transition-none before:hidden hover:bg-active hover:text-opacity-100',
         isActive &&
           'after:absolute after:left-3 after:h-1/3 after:w-0.5 after:-translate-x-1/2 after:rounded-full after:bg-primary',
         className
