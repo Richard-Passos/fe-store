493beccd75336aceb4025d93d4549aa9349e7ef3
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 18:51:12 -0300

    CHORE add dateFnsLocales

--- src/navigation.js
+++ src/navigation.js
@@ -1,9 +1,14 @@
+import { enUS, ptBR } from 'date-fns/locale';
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
-const locales = ['en', 'pt'];
+const locales = ['en', 'pt'],
+  dateFnsLocales = {
+    en: enUS,
+    pt: ptBR
+  };
 
 const sharedPathnamesNavigation = createSharedPathnamesNavigation({ locales });
 
-export { locales };
+export { locales, dateFnsLocales };
 export const { Link, redirect, usePathname, useRouter } =
   sharedPathnamesNavigation;
