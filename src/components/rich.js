ee503424d7391e3dd7da164cb98f2ccee797c290
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 18:50:18 -0300

    CHORE add text-content

--- src/components/rich.js
+++ src/components/rich.js
@@ -1,8 +1,8 @@
 import { Link } from '@/components/atoms';
 
 const rich = {
-  b: (chunks) => <b className='font-semibold'>{chunks}</b>,
-  i: (chunks) => <i className='font-medium italic'>{chunks}</i>,
+  b: (chunks) => <b className='font-medium text-content'>{chunks}</b>,
+  i: (chunks) => <i className='font-medium italic text-content'>{chunks}</i>,
   a: (chunks) => <Link>{chunks}</Link>,
   ul: (chunks) => <ul className='my-sm list-disc space-y-1 pl-sm'>{chunks}</ul>,
   ol: (chunks) => (
