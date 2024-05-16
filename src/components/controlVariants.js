2ae4d9a412ecb88ae6604345634d2980b866382d
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:12:20 -0300

    REFA

--- src/components/controlVariants.js
+++ src/components/controlVariants.js
@@ -1,19 +1,20 @@
 import { cnv } from '@/utils';
 
-const controlTypes = {
+const controlVariants = {
   solid: 'bg-main text-content hover:bg-active',
-  outline: 'bg-main text-content border-border hover:border-active'
+  outline:
+    'bg-main text-content border-border hover:border-[hsl(var(--root-content)/calc(var(--border-opacity)*2))]'
 };
 
-const controlVariants = cnv({
-  base: 'group flex h-10 w-full cursor-pointer rounded-sm border px-4 text-sm transition-colors placeholder:text-content/75 disabled:cursor-not-allowed disabled:opacity-50',
+const control = cnv({
+  base: 'group flex h-12 w-full cursor-pointer rounded-sm border border-transparent px-4 text-sm transition-colors placeholder:text-content/75 focus:outline disabled:cursor-not-allowed disabled:opacity-50',
   variants: {
-    type: controlTypes
+    variant: controlVariants
   },
   defaultVariants: {
-    type: 'solid'
+    variant: 'outline'
   }
 });
 
-export default controlVariants;
-export { controlVariants, controlTypes };
+export default control;
+export { control, controlVariants };
