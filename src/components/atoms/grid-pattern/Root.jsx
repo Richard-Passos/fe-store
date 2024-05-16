3b0ff958536101099dee9e70a21a6982b24f1054
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:14:00 -0300

    FIX color to border

--- src/components/atoms/grid-pattern/Root.jsx
+++ src/components/atoms/grid-pattern/Root.jsx
@@ -4,8 +4,8 @@ const GridPattern = ({ className, ...props }) => {
   return (
     <span
       className={cn(
-        'absolute inset-0 -z-40 overflow-hidden border bg-main bg-center [--c:hsl(var(--color)/var(--opacity))] [--color:--border] [--opacity:theme(opacity.80)] [background-image:linear-gradient(var(--c)_.8px,transparent_.8px),linear-gradient(to_right,var(--c)_.8px,transparent_.8px)] [background-size:180px_180px] [border-color:--c] dark:[--opacity:theme(opacity.40)]',
-        className,
+        'absolute inset-0 -z-40 overflow-hidden border bg-center [background-image:linear-gradient(theme(colors.border)_.8px,transparent_.8px),linear-gradient(to_right,theme(colors.border)_.8px,transparent_.8px)] [background-size:180px_180px]',
+        className
       )}
       {...props}
     />
