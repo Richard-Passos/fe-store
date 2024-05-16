a41de7bec37236f0a4ca0a219cf3db6da384a0a6
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:28:33 -0300

    REFA prettier

--- src/components/atoms/timerbar/Root.jsx
+++ src/components/atoms/timerbar/Root.jsx
@@ -3,32 +3,37 @@
 import { Root } from '@radix-ui/react-progress';
 import { forwardRef, useState } from 'react';
 
-import { useTimer } from '@/hooks';
 import { TimerbarProvider } from '@/contexts';
+import { useTimer } from '@/hooks';
+
 import variants from './variants';
 
 const TIMERBAR_UPDATE_DELAY = 100;
 
-const Timerbar = ({ isPaused, duration = 5000, color, size, className, ...props }, ref) => {
+const Timerbar = (
+  { isPaused, duration = 5000, color, size, className, ...props },
+  ref
+) => {
   const [value, setValue] = useState(duration - TIMERBAR_UPDATE_DELAY);
 
-  const handleSetValue = () => setValue((value) => value > 0 ? value - TIMERBAR_UPDATE_DELAY : 0)
+  const handleSetValue = () =>
+    setValue((value) => (value > 0 ? value - TIMERBAR_UPDATE_DELAY : 0));
 
   useTimer(isPaused, handleSetValue, duration - TIMERBAR_UPDATE_DELAY);
 
   return (
-      <TimerbarProvider value={{ value, duration }}>
-        <Root
+    <TimerbarProvider value={{ value, duration }}>
+      <Root
+        className={variants({ color, size, className })}
         max={duration}
-        value={value}
         min={0}
         ref={ref}
-        className={variants({ color, size, className })}
+        value={value}
         {...props}
       />
-      </TimerbarProvider>
+    </TimerbarProvider>
   );
 };
 
 export default forwardRef(Timerbar);
-export { TIMERBAR_UPDATE_DELAY};
+export { TIMERBAR_UPDATE_DELAY };
