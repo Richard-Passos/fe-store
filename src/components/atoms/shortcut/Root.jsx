e2cdca79b248a10633b3a68fbd6ef34a02f8b87a
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:22:56 -0300

    CHORE add text-content-75

--- src/components/atoms/shortcut/Root.jsx
+++ src/components/atoms/shortcut/Root.jsx
@@ -3,7 +3,10 @@ import { cn } from '@/utils';
 const Shortcut = ({ className, ...props }) => {
   return (
     <span
-      className={cn('ml-auto text-xs tracking-widest opacity-75', className)}
+      className={cn(
+        'ml-auto text-xs tracking-widest text-content/75',
+        className
+      )}
       {...props}
     />
   );
