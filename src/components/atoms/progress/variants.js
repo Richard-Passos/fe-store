0468d2453d6bda4e54357a1f63d680aa9645dafb
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:17:25 -0300

    CHORE rename

--- src/components/atoms/progress/variants.js
+++ src/components/atoms/progress/variants.js
@@ -1,4 +1,3 @@
-import colorVariants from '@/components/colorVariants';
 import { cnv } from '@/utils';
 
 const progressSizes = {
@@ -9,10 +8,9 @@ const progressSizes = {
 };
 
 /* 'relative' and 'z-0' are to fix overflow clipping in Safari. See more here: https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0 */
-const progressVariants = cnv({
-  base: 'relative z-0 w-full overflow-hidden rounded-full bg-main/20',
+const progress = cnv({
+  base: 'relative z-0 w-full overflow-hidden rounded-full bg-main/10',
   variants: {
-    color: colorVariants,
     size: progressSizes
   },
   defaultVariants: {
@@ -21,5 +19,5 @@ const progressVariants = cnv({
   }
 });
 
-export default progressVariants;
-export { progressSizes, progressVariants };
+export default progress;
+export { progress, progressSizes };
