de92fc57a73c1f4b433d5efc17a99a1e3f8a0488
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-15 23:28:49 -0300

    CHORE rename

--- src/components/atoms/timerbar/variants.js
+++ src/components/atoms/timerbar/variants.js
@@ -1,4 +1,3 @@
-import colorVariants from '@/components/colorVariants';
 import { cnv } from '@/utils';
 
 const timerbarSizes = {
@@ -9,10 +8,9 @@ const timerbarSizes = {
 };
 
 /* 'relative' and 'z-0' are to fix overflow clipping in Safari. See more here: https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0 */
-const timerbarVariants = cnv({
+const timerbar = cnv({
   base: 'relative z-0 w-full overflow-hidden rounded-full bg-main/20',
   variants: {
-    color: colorVariants,
     size: timerbarSizes
   },
   defaultVariants: {
@@ -21,5 +19,5 @@ const timerbarVariants = cnv({
   }
 });
 
-export default timerbarVariants;
-export { timerbarSizes, timerbarVariants };
+export default timerbar;
+export { timerbar, timerbarSizes };
