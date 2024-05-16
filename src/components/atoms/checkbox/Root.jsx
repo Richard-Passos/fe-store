850e47534f266a1b9d79af0729048f899e4b6e98
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-16 15:22:49 -0300

    CHORE add control

--- src/components/atoms/checkbox/Root.jsx
+++ src/components/atoms/checkbox/Root.jsx
@@ -3,14 +3,19 @@
 import { Root } from '@radix-ui/react-checkbox';
 import { forwardRef } from 'react';
 
+import control from '@/components/controlVariants';
 import { cn } from '@/utils';
 
-const Checkbox = ({ className, ...props }, ref) => (
+const Checkbox = ({ color, variant, className, ...props }, ref) => (
   <Root
-    className={cn(
-      'inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-sm border border-primary text-primary-content disabled:cursor-not-allowed disabled:opacity-50 data-checked:bg-primary',
-      className,
-    )}
+    className={control({
+      color,
+      variant,
+      className: cn(
+        'size-5 shrink-0 items-center justify-center px-0',
+        className
+      )
+    })}
     ref={ref}
     {...props}
   />
