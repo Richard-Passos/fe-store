d67ed01c6dafabd25d68e69494438e56ec5943e0
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-16 16:30:43 -0300

    CHORE add replace to --

--- src/utils/normId.js
+++ src/utils/normId.js
@@ -1,9 +1,7 @@
 const normId = (str) =>
-  str?.[0]?.toLowerCase() +
-  str
-    ?.slice(1)
-    .replace(/([A-Z])/g, '-$1')
-    ?.replace?.(/[ _]]/g, '-')
+  (str?.[0]?.toLowerCase() + str?.slice(1).replace(/([A-Z])/g, '-$1'))
+    ?.replace?.(/[ _]/g, '-')
+    .replace('--', '-')
     .toLowerCase();
 
 export default normId;
