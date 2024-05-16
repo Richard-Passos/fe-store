a9fb37a956fa450339dfa47fcac1c76ba177f76f
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-16 01:26:11 -0300

    CHORE add state

--- src/contexts/Timerbar.jsx
+++ src/contexts/Timerbar.jsx
@@ -1,11 +1,40 @@
 'use client';
 
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const TimerbarContext = createContext({ value: 0, duration: 0 });
+import { useInterval } from '@/hooks';
 
-const TimerbarProvider = (props) => {
-  return <TimerbarContext.Provider {...props} />;
+const TIMERBAR_UPDATE_DELAY = 100;
+
+const TimerbarContext = createContext({
+  activeValue: 0,
+  setActiveValue: () => {},
+  duration: 0
+});
+
+const TimerbarProvider = ({ duration, isPaused, value, ...props }) => {
+  const [activeValue, setActiveValue] = useState(
+    duration - TIMERBAR_UPDATE_DELAY
+  );
+
+  useInterval(
+    () =>
+      setActiveValue((value) =>
+        value > 0 ? value - TIMERBAR_UPDATE_DELAY : 0
+      ),
+    isPaused ? null : TIMERBAR_UPDATE_DELAY
+  );
+
+  return (
+    <TimerbarContext.Provider
+      value={{
+        activeValue,
+        duration,
+        ...value
+      }}
+      {...props}
+    />
+  );
 };
 
 export default TimerbarContext;
