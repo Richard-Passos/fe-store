724b312519bea779c86e142d583c156714cfa2ea
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-18 11:40:07 -0300

    CHORE export

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,5 +1,6 @@
 export { default as Accordions } from './accordions';
 export { default as Cards } from './cards';
+export { default as Combobox } from './combobox';
 export { default as ComponentsNav } from './components-nav';
 export { default as Cta } from './cta';
 export { default as Demos } from './demos';
