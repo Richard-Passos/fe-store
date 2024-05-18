fe6055e0f46da77226cee4d105339b637fc29733
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-18 11:35:06 -0300

    CHORE add size

--- src/components/atoms/input/Root.jsx
+++ src/components/atoms/input/Root.jsx
@@ -3,12 +3,13 @@ import { forwardRef } from 'react';
 import control from '@/components/controlVariants';
 import { cn } from '@/utils';
 
-const Input = ({ color, variant, className, ...props }, ref) => {
+const Input = ({ color, variant, size, className, ...props }, ref) => {
   return (
     <input
       className={control({
         color,
         variant,
+        size,
         className: cn(
           'file:mr-2 file:mt-2 file:cursor-pointer file:border-0 file:bg-transparent file:px-0 file:font-medium file:text-content disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-0 [&[variant=color]]:overflow-hidden [&[variant=color]]:px-0 [&[variant=file]]:text-content/75',
           className
