1eb0b6ae351fea0c5920ca2d128ead3474d3d4d2
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-19 12:55:44 -0300

    CHORE add generic style

--- src/components/organisms/demos/accordion/Root.jsx
+++ src/components/organisms/demos/accordion/Root.jsx
@@ -1,7 +1,8 @@
 import { useTranslations } from 'next-intl';
 
 import animations from '@/components/animations';
-import { Icon } from '@/components/atoms';
+import { Box, Button, Icon } from '@/components/atoms';
+import { TextDescription } from '@/components/atoms/text';
 import { Accordion } from '@/components/molecules';
 import { capitalize, cn, normKey, translationKeys } from '@/utils';
 import variantsComb from '@/utils/variantsComb';
@@ -23,41 +24,58 @@ const ACCORDION = [
 const DemosAccordionOrganism = ({ namespace, ...props }) => {
   const t = useTranslations(namespace);
 
-  return ACCORDION.map((variants, i) =>
-    variantsComb(variants).map((variants, idx) => (
+  return ACCORDION.map((variants) =>
+    variantsComb(variants).map((variants, i) => (
       <Demos
-        key={`${i}${idx}`}
+        key={i}
         variants={variants}
         {...props}
       >
         <Accordion.Root {...variants}>
           {translationKeys(t, 'items').map((key) => (
             <Accordion.Item
-              className='rounded-sm'
-              color={t('color')}
+              asChild
               key={key}
               value={`item-${key}`}
             >
-              <Accordion.Header>
-                <Accordion.Trigger>
-                  {capitalize(t(`items.${key}.title`))}
+              <Box
+                className='rounded-sm p-0'
+                color={t('color')}
+                variant={t('variant')}
+              >
+                <Accordion.Header>
+                  <Accordion.Trigger asChild>
+                    <Button
+                      className='group w-full justify-between rounded-none border-0 text-start'
+                      color={t('trigger.color')}
+                      size={t('trigger.size')}
+                      variant={t('trigger.variant')}
+                    >
+                      {capitalize(t(`items.${key}.title`))}
 
-                  <div className='size-4'>
-                    <Icon
-                      className={cn(
-                        'group-data-open:animate-active',
-                        animations[normKey(t('icon.animation'))]
-                      )}
-                      color={t('icon.color')}
-                      src={t('icon.src')}
-                    />
-                  </div>
-                </Accordion.Trigger>
-              </Accordion.Header>
+                      <div className='size-4'>
+                        <Icon
+                          className={cn(
+                            'group-data-open:animate-active',
+                            animations[normKey(t('trigger.icon.animation'))]
+                          )}
+                          color={t('trigger.icon.color')}
+                          src={t('trigger.icon.src')}
+                        />
+                      </div>
+                    </Button>
+                  </Accordion.Trigger>
+                </Accordion.Header>
 
-              <Accordion.Content>
-                {t(`items.${key}.description`)}
-              </Accordion.Content>
+                <Accordion.Content>
+                  <TextDescription
+                    asChild
+                    className='p-md text-sm'
+                  >
+                    <div>{t.rich(`items.${key}.description`)}</div>
+                  </TextDescription>
+                </Accordion.Content>
+              </Box>
             </Accordion.Item>
           ))}
         </Accordion.Root>
