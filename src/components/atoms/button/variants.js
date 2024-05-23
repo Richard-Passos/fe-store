f691ce2792859088bfc1fa03e77c59d90a5c2ca1
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-23 12:00:01 -0300

    CHORE styling

--- src/components/atoms/button/variants.js
+++ src/components/atoms/button/variants.js
@@ -8,12 +8,12 @@ const buttonVariants = {
   buttonSizes = {
     xs: 'h-8 px-xs gap-2xs text-xs',
     sm: 'h-10 px-sm gap-xs text-sm',
-    md: 'h-12 px-md gap-sm text-sm',
-    lg: 'h-14 px-lg gap-sm'
+    md: 'h-12 px-sm gap-sm text-sm',
+    lg: 'h-14 px-md gap-sm'
   };
 
 const button = cnv({
-  base: 'relative inline-flex items-center justify-center rounded-sm border border-transparent text-center font-medium transition-colors [--outline-color:--root-main] first-letter:uppercase focus-visible:outline-[--outline-color] disabled:pointer-events-none disabled:opacity-50 [&:has(>svg:only-child)]:aspect-square [&:has(>svg:only-child)]:px-0',
+   base: 'relative inline-flex items-center justify-center rounded-sm border border-transparent text-center font-medium transition-colors [--outline-color:theme(colors.main)] first-letter:uppercase focus-visible:outline-[--outline-color] disabled:pointer-events-none disabled:opacity-50 [&:has(>svg:only-child)]:aspect-square [&:has(>svg:only-child)]:px-0',
   variants: {
     variant: buttonVariants,
     size: buttonSizes
@@ -31,7 +31,7 @@ const button = cnv({
     },
     {
       color: ['main', 'muted', 'inherit'],
-      className: '[--outline-color:--root-content]'
+      className: '[--outline-color:theme(colors.content)]'
     }
   ],
   defaultVariants: {
