d5330ed6aaa4c8a9a04557f87651ce92cb223919
Richard Passos <richard.ac.passos12@gmail.com>
2024-05-23 12:00:40 -0300

    FIX outline-color

--- src/components/atoms/control/variants.js
+++ src/components/atoms/control/variants.js
@@ -13,7 +13,7 @@ const controlVariants = {
   };
 
 const control = cnv({
-  base: 'group inline-flex w-full cursor-pointer items-center justify-between rounded-sm border border-transparent px-sm font-medium transition-colors [--outline-color:--root-main] placeholder:font-normal placeholder:text-content/75 focus:outline-[--outline-color] disabled:cursor-not-allowed disabled:opacity-50',
+  base: 'group inline-flex w-full cursor-pointer items-center justify-between rounded-sm border border-transparent px-sm font-medium transition-colors [--outline-color:theme(colors.main)] placeholder:font-normal placeholder:text-content/75 focus:outline-[--outline-color] disabled:cursor-not-allowed disabled:opacity-50',
   variants: {
     variant: controlVariants,
     size: controlSizes
@@ -21,7 +21,7 @@ const control = cnv({
   compoundVariants: [
     {
       color: ['main', 'muted', 'inherit'],
-      className: '[--outline-color:--root-content]'
+      className: '[--outline-color:theme(colors.content)]'
     }
   ],
   defaultVariants: {
